9b03bed9ea42050884a307fbbb2a3d2297ce0ab6
Binod Marikhu <bnod_468@hotmail.com>
2017-06-01 12:27:08 +0545

    Add project files.

---
+++ GR.Mvc.UI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GR.Mvc.UI.Startup))]
+namespace GR.Mvc.UI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
