6586f063f7aebadc85794d2f4441d112613ab9f8
Binod Marikhu <bnod_468@hotmail.com>
2017-06-01 13:00:55 +0545

    Generic Repository created

---
+++ GR.Repository/IRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq;
+
+namespace GR.Repository
+{
+    public interface IRepository<T> where T:class
+    {
+        void Create(T entity);
+        IQueryable<T> Read();
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
---
+++ GR.Repository/Repository.cs
@@ -0,0 +1,40 @@
+﻿using GR.Data;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GR.Repository
+{
+    class Repository<T> : IRepository<T> where T : class
+    {
+        private ForumContext db = new ForumContext();
+        public void Create(T entity)
+        {
+            //db.Users.Add(users);
+            db.Set<T>().Add(entity);
+            db.SaveChanges();
+        }
+
+        public void Delete(T entity)
+        {
+            //db.Users.Remove(users);
+            db.Set<T>().Remove(entity);
+            db.SaveChanges();
+        }
+
+        public IQueryable<T> Read()
+        {
+            return db.Set<T>().AsQueryable();
+        }
+
+        public void Update(T entity)
+        {
+            //db.Entry(users).State = EntityState.Modified;
+            db.Entry(entity).State = EntityState.Modified;
+            db.SaveChanges();
+        }
+    }
+}
